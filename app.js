d18b9d03698b1188c701339c12513b4046ea2a99
Gabriela Cepeda <gabcep14@gmail.com>
2020-02-20 22:06:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 
-// const dummyTransactions = [
-//   { id: 1, text: "Flowers", amount: -20 },
-//   { id: 2, text: "Salary", amount: 300 },
-//   { id: 3, text: "Book", amount: -10 },
-//   { id: 4, text: "Camera", amount: 150 }
-// ];
 
 const localStorageTransactions = JSON.parse(
   localStorage.getItem("transactions")
